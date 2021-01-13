bf29bab6fd88ec6e2e4d07c4cb1939412374792e
shigekuni <zolotorevvp@gmail.com>
2021-01-14 00:08:55 +0500

    First commit. Echo bot message.

---
+++ server.py
@@ -0,0 +1,31 @@
+import logging
+import os
+
+from aiogram import Bot, Dispatcher, executor, types
+
+logging.basicConfig(level=logging.INFO)
+
+API_TOKEN = os.getenv("TELEGRAM_API_TOKEN")
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    # old style:
+    # await bot.send_message(message.chat.id, message.text)
+
+    await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
